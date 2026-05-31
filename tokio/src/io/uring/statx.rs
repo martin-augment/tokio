b0bc6ee778b5e7ef3b11fd8215eaaf3cf6f28599
Mahdi Ali-Raihan <asder8215@gmail.com>
2026-05-31 02:07:02 -0400

    Remove unnecessary utils function, refactored code in statx and statx fd leak test, and removed cfg_io_uring gates (localized the feature gate to the pertinent part of the code that uses it in read_uring)

--- tokio/src/io/uring/statx.rs
+++ tokio/src/io/uring/statx.rs
@@ -16,10 +16,11 @@
 ))]
 
 use crate::fs::File;
-use crate::io::uring::utils::{box_assume_init, box_new_uninit, cstr};
+use crate::io::uring::utils::cstr;
 use crate::runtime::driver::op::{CancelData, Cancellable, Completable, CqeResult, Op};
 use io_uring::{opcode, types};
 use libc::statx;
+use std::ffi::{CStr, CString};
 use std::fmt::{Debug, Formatter};
 use std::io;
 use std::mem::MaybeUninit;
@@ -48,7 +49,7 @@ pub(crate) struct Statx {
     /// This field will be read by the kernel during the operation, so we
     /// need to ensure it is valid for the entire duration of the operation.
     #[allow(dead_code)]
-    path: std::ffi::CString,
+    path: CString,
     buffer: Box<MaybeUninit<statx>>,
 }
 
@@ -58,12 +59,11 @@ impl Completable for Statx {
     fn complete(self, cqe: CqeResult) -> Self::Output {
         // SAFETY: On success, we always receive 0, which should guarantee
         // that the information about a file is stored inside the
-        // statx buffer. On failure, we'll receive an Error value,
-        // avoiding misuse of `.box_assume_init()`.
+        // statx buffer. On failure, we'll receive an Error value.
         // Refer to man page description and return value:
         // https://man7.org/linux/man-pages/man2/statx.2.html
         cqe.result
-            .map(|_| Metadata(*unsafe { box_assume_init(self.buffer) }))
+            .map(|_| Metadata(unsafe { *self.buffer.as_ptr() }))
     }
 
     fn complete_with_error(self, error: io::Error) -> Self::Output {
@@ -82,7 +82,7 @@ impl Op<Statx> {
     #[inline]
     fn statx(path: &Path, flags: i32) -> io::Result<Op<Statx>> {
         let path = cstr(path)?;
-        let mut buffer = box_new_uninit::<statx>();
+        let mut buffer = Box::new(MaybeUninit::<statx>::uninit());
 
         let statx_op = opcode::Statx::new(
             types::Fd(libc::AT_FDCWD),
@@ -106,8 +106,8 @@ impl Op<Statx> {
 
     /// Retrieves the metadata information of the given file
     pub(crate) fn file_metadata(file: &File) -> io::Result<Op<Statx>> {
-        let mut buffer = box_new_uninit::<statx>();
-        let empty_path = cstr(Path::new(""))?;
+        let mut buffer = Box::new(MaybeUninit::<statx>::uninit());
+        let empty_path: &'static CStr = c"";
 
         // io-uring was introduced in linux 5.1
         // pass in an empty path instead of null to target the file descriptor
@@ -130,7 +130,7 @@ impl Op<Statx> {
             Op::new(
                 statx_op,
                 Statx {
-                    path: empty_path,
+                    path: empty_path.into(),
                     buffer,
                 },
             )
